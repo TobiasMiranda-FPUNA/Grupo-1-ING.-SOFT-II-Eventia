77d082f40f8972855ccedc9e8358d5f392353f5a
TobiasMiranda-FPUNA <mongelostobias03@fpuna.edu.py>
2026-09-24 21:55:13 -0300

    Add files via upload

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Roles } from './pages/roles/roles';
+import { Eventos } from './pages/eventos/eventos';
+import { EventoForm } from './pages/evento-form/evento-form';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: Login },
-    { path: 'roles', component: Roles }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'roles', component: Roles },
+  { path: 'eventos', component: Eventos },
+  { path: 'eventos/nuevo', component: EventoForm },
+  { path: 'eventos/:id/editar', component: EventoForm },
+  { path: '**', redirectTo: 'login' },
 ];
